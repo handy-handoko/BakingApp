f25b1f930256c32a73c6d4295497b45ebfc381ab
Luqman Fanani <luqman.fanani@gmail.com>
2017-08-30 19:44:31 +0800

    recipe step detail fragment

--- app/src/main/java/com/digitalnusantarastudio/bakingapp/fragments/StepDetailFragment.java
+++ app/src/main/java/com/digitalnusantarastudio/bakingapp/fragments/StepDetailFragment.java
@@ -1,16 +1,28 @@
 package com.digitalnusantarastudio.bakingapp.fragments;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.digitalnusantarastudio.bakingapp.R;
+import com.digitalnusantarastudio.bakingapp.adapter.RecipeStepRecyclerViewAdapter;
+
+import java.io.IOException;
+
+import butterknife.BindView;
 
 public class StepDetailFragment extends Fragment {
+    @BindView(R.id.txtDescription) TextView txtDescription;
+    @BindView(R.id.videoView) VideoView videoView;
 
     public StepDetailFragment() {
         // Required empty public constructor
@@ -21,6 +33,17 @@ public class StepDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_step_detail, container, false);
+        String url = "http://go.udacity.com/android-baking-app-json";
+        Uri uri = Uri.parse(url);
+        videoView.setVideoURI(uri);
+
+//        try {
+//            MediaPlayer mediaPlayer = new MediaPlayer();
+//            mediaPlayer.setDataSource(getActivity(), uri);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        return view;
     }
 }
