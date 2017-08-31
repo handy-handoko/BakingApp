f309d32aa778d605d4165e4f1b05b5ca3d64c85a
Luqman Fanani <luqman.fanani@gmail.com>
2017-08-31 16:36:14 +0800

    send data and navigation on recipe detail

--- app/src/main/java/com/digitalnusantarastudio/bakingapp/activities/StepDetailActivity.java
+++ app/src/main/java/com/digitalnusantarastudio/bakingapp/activities/StepDetailActivity.java
@@ -1,15 +1,66 @@
 package com.digitalnusantarastudio.bakingapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.digitalnusantarastudio.bakingapp.R;
+import com.digitalnusantarastudio.bakingapp.fragments.StepDetailFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class StepDetailActivity extends AppCompatActivity {
+    private JSONArray steps_json_array;
+    private int current_step = 0;
+    private StepDetailFragment stepDetailFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_detail);
+
+        Intent intent = getIntent();
+        String steps_json_string = intent.getStringExtra(getString(R.string.steps_json_key));
+        try {
+            steps_json_array = new JSONArray(steps_json_string);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        stepDetailFragment = (StepDetailFragment)getSupportFragmentManager().findFragmentById(R.id.step_detail_fragment);
+    }
+
+    public void prev(View view){
+        if(current_step == 0){
+            Toast.makeText(this, "This is first step", Toast.LENGTH_SHORT).show();
+        }else{
+            current_step -= 1;
+            load_data();
+        }
+    }
+
+    public void next(View view){
+        if(current_step == steps_json_array.length()-1){
+            Toast.makeText(this, "This is last step", Toast.LENGTH_SHORT).show();
+        }else{
+            current_step += 1;
+            load_data();
+        }
+    }
+
+    private void load_data(){
+        JSONObject step_json_object;
+        try {
+            step_json_object = steps_json_array.getJSONObject(current_step);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            //if error occured, show toast message and return to avoid crash
+            Toast.makeText(this, "An error occured", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        stepDetailFragment.showStep(step_json_object);
     }
 }
--- app/src/main/java/com/digitalnusantarastudio/bakingapp/fragments/StepDetailFragment.java
+++ app/src/main/java/com/digitalnusantarastudio/bakingapp/fragments/StepDetailFragment.java
@@ -1,29 +1,27 @@
 package com.digitalnusantarastudio.bakingapp.fragments;
 
-import android.content.Context;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.digitalnusantarastudio.bakingapp.R;
-import com.digitalnusantarastudio.bakingapp.adapter.RecipeStepRecyclerViewAdapter;
 
-import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 
 public class StepDetailFragment extends Fragment {
-    @BindView(R.id.txtDescription) TextView txtDescription;
-    @BindView(R.id.videoView) VideoView videoView;
-
+//    @BindView(R.id.txtDescription) TextView txtDescription;
+    TextView txtDescription;
+    VideoView videoView;
     public StepDetailFragment() {
         // Required empty public constructor
     }
@@ -34,16 +32,17 @@ public class StepDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_step_detail, container, false);
-        String url = "http://go.udacity.com/android-baking-app-json";
-        Uri uri = Uri.parse(url);
-        videoView.setVideoURI(uri);
+        txtDescription = (TextView) view.findViewById(R.id.txtDescription);
 
-//        try {
-//            MediaPlayer mediaPlayer = new MediaPlayer();
-//            mediaPlayer.setDataSource(getActivity(), uri);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         return view;
+    }
+
+    public void showStep(JSONObject jsonObject){
+        try {
+            txtDescription.setText(jsonObject.getString("description"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
