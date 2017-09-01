96f0de284c79dfdd2c72bf90b24960c75acdd666
Luqman Fanani <luqman.fanani@gmail.com>
2017-09-01 18:53:56 +0800

    ingredients activity

---
+++ app/src/main/java/com/digitalnusantarastudio/bakingapp/activities/IngredientsActivity.java
@@ -0,0 +1,15 @@
+package com.digitalnusantarastudio.bakingapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.digitalnusantarastudio.bakingapp.R;
+
+public class IngredientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients);
+    }
+}
