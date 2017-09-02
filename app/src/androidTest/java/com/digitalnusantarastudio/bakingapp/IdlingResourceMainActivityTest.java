73f2725a47d288e409541afde700d84632fd9b43
Luqman Fanani <luqman.fanani@gmail.com>
2017-09-03 07:34:21 +0800

    check if click open right recipe

--- app/src/androidTest/java/com/digitalnusantarastudio/bakingapp/IdlingResourceMainActivityTest.java
+++ app/src/androidTest/java/com/digitalnusantarastudio/bakingapp/IdlingResourceMainActivityTest.java
@@ -2,23 +2,29 @@ package com.digitalnusantarastudio.bakingapp;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.filters.LargeTest;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.anything;
+import android.support.v7.widget.Toolbar;
 
 import com.digitalnusantarastudio.bakingapp.activities.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by luqman on 02/09/17.
@@ -46,6 +52,9 @@ public class IdlingResourceMainActivityTest {
         //thanks to gabor and jdebon
         //https://stackoverflow.com/questions/27396583/how-to-click-on-an-item-inside-a-recyclerview-in-espresso
         onView(withId(R.id.recipe_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        CharSequence title = "Nutella Pie";
+        matchToolbarTitle(title);
     }
 
     @After
@@ -53,5 +62,22 @@ public class IdlingResourceMainActivityTest {
         if (mIdlingResource != null) {
             Espresso.unregisterIdlingResources(mIdlingResource);
         }
+    }
+
+    private static ViewInteraction matchToolbarTitle(CharSequence title) {
+        return onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+    }
+
+    private static Matcher<Object> withToolbarTitle(final Matcher<CharSequence> textMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+            @Override public boolean matchesSafely(Toolbar toolbar) {
+                return textMatcher.matches(toolbar.getTitle());
+            }
+            @Override public void describeTo(Description description) {
+                description.appendText("with toolbar title: ");
+                textMatcher.describeTo(description);
+            }
+        };
     }
 }
